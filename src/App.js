deb5065b6f3bb04ba79bc208a6d9a2092beba444
Micheala-H <mholthus17@gmail.com>
2023-08-17 17:12:09 -0500

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <div className="container mx-auto bg-gray-200 rounded-xl">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+      Hallo
+      </p>
+      <p className="text-gray-500 text-lg">
+        react here!
+      </p>
+  </div>
   );
 }
 
