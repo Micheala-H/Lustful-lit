08800b562a0346dd8f7e756679a6ab61669880e2
Micheala-H <mholthus17@gmail.com>
2023-08-28 17:24:22 -0500

    added title and basic start to background

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './index.css'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-color-amber">
-      Hello world!
-    </h1>
+    <div className="bg-fixed bg-cover" style={{backgroundImage:"url(https://images.unsplash.com/photo-1493673272479-a20888bcee10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9uc3RlcmElMjBsZWFmfGVufDB8fDB8fHww&w=1000&q=80)"}}>
+      <h1 className="text-6xl font-bold text-fuchsia-500 text-center">
+        Lustful Lit
+      </h1>
+      
+    </div>
   )
 }
