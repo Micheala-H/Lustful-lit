8195eb77eb3e5a0b5b8fc05419fbf80c29baa3ea
Micheala-H <mholthus17@gmail.com>
2023-08-28 14:21:34 -0500

    Added tailiwind CSS to make all visuals

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-function App() {
-  return(
-    <div className="container mx-auto bg-gray-200 rounded-xl">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-      Hallo
-      </p>
-      <p className="text-gray-500 text-lg">
-        react here!
-      </p>
-  </div>
-  );
-}
+import './index.css'
 
-export default App;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-color-amber">
+      Hello world!
+    </h1>
+  )
+}
